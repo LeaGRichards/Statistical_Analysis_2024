b0a256b347f3bf3867072d31bc7f5914edd87991
LeaGRichards <lea.giguere.richards@gmail.com>
2025-02-16 22:01:00 +0100

    Update FINAL_PROJECT.R

--- FINAL_PROJECT.R
+++ FINAL_PROJECT.R
@@ -10,7 +10,7 @@
 
 # Setting the working directory 
   getwd() # Informs us where the working directory currently is
-  setwd("/Users/lea/Desktop/R") # Sets your working 
+  setwd("/Users/lea/Desktop/R") # Sets your working directory
     # Place working directory location in quotation marks
   
 # Installing and calling the necessary packages and libraries for the project
@@ -145,8 +145,8 @@
  ########## Question 3 ##########
  ################################
  
-# Testing for normality of distribution of the sepal width data
-  shapiro.test(iris$Sepal.Width)
+# Testing for normality of distribution of the error terms
+  shapiro.test(resid(sepal_lm)
   # The p-value = 0.1012
   
 # Making a histogram to visualize the distribution of the sepal width data
@@ -155,7 +155,7 @@
   hist(iris$Sepal.Width, # Using the sepal width of the iris dataset
        xlab = "Setal Width", # Adds a x axis label
        main = "Density-based histogram of sepal width", # Adds a title
-       freq = FALSE) # The histogram will plot the probability densities rather then the frequencies
+       freq = FALSE) # The histogram will plot the probability densities rather than the frequencies
   
   # Adding the associated density line to the histogram
   lines(density(iris$Sepal.Width))
@@ -171,7 +171,7 @@
   hist(iris$Petal.Width, # Using the sepal width of the iris dataset
        xlab = "Petal Width", # Adds a x axis label
        main = "Density-based histogram of sepal width", # Adds a title
-       freq = FALSE) # The histogram will plot the probability densities rather then the frequencies
+       freq = FALSE) # The histogram will plot the probability densities rather than the frequencies
   
   # Adding the associated density line to the histogram
   lines(density(iris$Petal.Width))
@@ -259,3 +259,91 @@
   
   # Saving the multiframe as pdf file in the working directory
   dev.copy2pdf(file = "question4b.pdf")
+
+################################
+#### Addition to question 4 ####
+################################
+  
+# Definitions
+  iris.labels=(paste("Iris", iris$Species))     # create a new vector that adds 'Iris' to the species names
+  cluster_colors <- c("red", "green", "blue")   # create a vector with colors for the clusters 
+  # (3 colors for 3 clusters / 3 species)
+  
+  
+## Cluster Analysis ##
+
+# calculate euclidian distance on balanced data
+  iris.dist=dist(scale(iris[,-5]))
+  
+# complete linkage method
+  iris.hc=hclust(iris.dist)
+  plot(iris.hc, labels=iris.labels,                   # plot dendrogram with the predefined labels and define a labelsize
+       xlab="Cluster Index",                          # set custom x label
+       ylab="Height", cex=0.4,                        # set custom y label and set size of cluster inndices
+       main="Cluster Dendrogram (Complete Linkage)",  # set title
+       sub="")                                        # remove subtitle
+  rect.hclust(iris.hc, k=3, border=cluster_colors)    # plot rectangles for the 3 main clusters using the pre-defined colors
+  dev.copy2pdf(file="Cluster_hc.pdf")                 # save graph as pdf
+  iris.hc2 <- cutree(iris.hc, k=3)                    #'cut' the dendrogram and save the 3 different clusters
+  
+# average method
+  iris.ha=hclust(iris.dist, method="average")
+  plot(iris.ha, labels=iris.labels,                   # see above
+       xlab="Cluster Index",            
+       ylab="Height", cex=0.4,               
+       main="Cluster Dendrogram (Average)",           
+       sub="")                                        
+  rect.hclust(iris.ha, k=3, border=cluster_colors)
+  dev.copy2pdf(file="Cluster_ha.pdf")
+  iris.ha2 <- cutree(iris.ha, k=3)
+  
+# Ward's D method
+  iris.hw=hclust(iris.dist, method="ward.D")
+  plot(iris.hw, labels=iris.labels,                   # see above
+       xlab="Cluster Index",
+       ylab="Height", cex=0.4,             
+       main="Cluster Dendrogram (Ward's D)",       
+       sub="")                                    
+  rect.hclust(iris.hw, k=3, border=cluster_colors)
+  dev.copy2pdf(file="Cluster_hw.pdf")
+  iris.hw2 <- cutree(iris.hw, k=3)
+  
+# create tables to compare the clusters to the original species (saved as screenshot)
+  table(iris.hc2, iris$Species)     # Complete linkage vs. actual species
+  table(iris.ha2, iris$Species)     # Average linkage vs. actual species
+  table(iris.hw2, iris$Species)     # Ward's D vs. actual species
+  
+  
+## PCA analysis ##
+  pca1 <- dudi.pca(iris[,-5],          # [,-5] to exclude species
+                   center=T, scale=T,  # data balancing to render all variables numerically homogeneous and comparable
+                   scannf=F, nf = 2)   # automatically select and retain only the first two PCs
+  
+# create a new data frame to save values for plotting
+  pca_df <- data.frame(PC1 = pca1$li[,1],                    # first column are PC1 values
+                       PC2 = pca1$li[,2],                    # second column are PC2 values
+                       Species = paste("Iris",iris$Species)) # keep species column for plotting
+  
+# create a function to generate the recolored PCA plots
+  create_pca_plot <- function(pca_df, cluster_labels, plot_title) {   # define a function and the input parameters
+    pca_df$Cluster <- factor(cluster_labels)                          # add cluster labels to the data frame
+    ggplot(pca_df, aes(x=PC1, y=PC2, color=Cluster, shape=Species)) + # use ggplot to plot the PCA results, differentiate...
+      # ...species by symbol shape and species by color
+      geom_point(size=1.5) +                                          # set symbol size
+      labs(title=plot_title, x="PC1", y="PC2") +                      # set title and axis labels
+      scale_color_manual(values=cluster_colors) +                     # use the defined colors
+      theme_minimal()                                                 # choose a pre-set-theme for visualization
+  }
+  
+  
+## create the PCA plots for the PCA result and the three clustering methods
+  # use pca_df as input for the PCA plots and the above generated cluster data as labels to differentiate the clusters...
+  # ...by different colors - for the original PCA plot use the species column; and set the title of each subplot
+  original_pca_plot <- create_pca_plot(pca_df, pca_df$Species, "PCA (without cluster analysis)")
+  hc_pca_plot <- create_pca_plot(pca_df, iris.hc2, "PCA (Complete Linkage Clustering)")
+  ha_pca_plot <- create_pca_plot(pca_df, iris.ha2, "PCA (Average Linkage Clustering)")
+  hw_pca_plot <- create_pca_plot(pca_df, iris.hw2, "PCA (Ward's D Clustering)")
+  
+# use gridExtra to plot the original PCA and the 3 recolored ones in subplots
+  grid.arrange(original_pca_plot, hc_pca_plot, ha_pca_plot, hw_pca_plot, ncol=2)
+  dev.copy2pdf(file="PCA_3clusters.pdf") # save graph as pdf
